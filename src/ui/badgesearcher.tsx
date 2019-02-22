733392796232d6d49f0632e49d6e5f413b3c41df
Meno Abels <meno.abels@adviser.com>
2019-02-22 16:29:40 +0100

    chore: enable styled components

--- src/ui/badgesearcher.tsx
+++ src/ui/badgesearcher.tsx
@@ -1,8 +1,15 @@
-import Chip, { ChipProps } from '@material-ui/core/Chip';
 import React = require('react');
+import Chip, { ChipProps } from '@material-ui/core/Chip';
+import styled from '@emotion/styled';
+
+export const BadgeSearcherStyles = styled(Chip as React.SFC<ChipProps>)`
+  background: pink;
+`;
 
 export function BadgeSearcher(props: ChipProps): JSX.Element {
   return (
-    <Chip {...props} label={`badgesearcher(${props.label})`} />
+    <BadgeSearcherStyles {...props}
+          label={`badgesearcher(${props.label})`}
+    />
   );
 }
